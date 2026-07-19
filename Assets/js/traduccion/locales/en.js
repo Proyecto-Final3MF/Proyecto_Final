aaa3d846b1de80cb0274f32f905628d2adac9e8d
AlfreThiago <alfrethiagoc@gmail.com>
2026-07-19 16:50:42 -0300

    Update en.js

--- Assets/js/traduccion/locales/en.js
+++ Assets/js/traduccion/locales/en.js
@@ -2,19 +2,19 @@ export default {
     welcome: "Welcome to",
 
     //Categorias
-    Laptop: "Laptop",
-    Celular: "Cellphone",
+    "Laptop": "Laptop",
+    "Celular": "Cellphone",
     "Televisión": "TV",
-    Monitor: "Monitor",
-    Impresora: "Printer",
-    Consola: "VideoGame Console",
+    "Monitor": "Monitor",
+    "Impresora": "Printer",
+    "Consola": "VideoGame Console",
     "Router/Módem": "Router/Modem",
-    Smartwatch: "Smartwatch",
-    Camaras: "Cameras",
+    "Smartwatch": "Smartwatch",
+    "Camaras": "Cameras",
     "Disco Duro": "Hard Disc",
 
     //Estados
-    Disponible: "Availible",
+    "Disponible": "Availible",
     "En Diagnostico": "In Diagnosis",
     "En espera de repuestos": "Waiting for rapair parts",
     "En reparación": "Under repair",
