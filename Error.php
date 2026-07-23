152433b013311936dda31fa164c9d21ccc85b0ca
AlfreThiago <alfrethiagoc@gmail.com>
2026-07-23 04:24:01 -0300

    Update Error.php

--- Error.php
+++ Error.php
@@ -31,13 +31,13 @@ This is a modifieded version -->
     <link rel="icon" type="image/png" href="Assets/imagenes/logonueva.png">
 </head>
 <body class="error">
-    <div class="noise"></div>
+<div class="noise"></div>
 <div class="overlay1"></div>
 <div class="terminal">
-  <h2 class="h2_error">Error <span class="errorcode">404</span></h2>
-  <p1 class="output">> Es posible que la página que estás buscando haya sido eliminada, no exista, haya cambiado de nombre o no esté disponible temporalmente.</p>
-  <p2 class="output">> Podes intentar volver a <a class="errorLink" href="Index.php?accion=inicio">[Pagina Inicial]</a>.</p>
-  <p3 class="output">> Gracias por la comprensión.</p>
+    <h2 class="h2_error">Error <span class="errorcode">404</span></h2>
+    <p1 class="output">> Es posible que la página que estás buscando haya sido eliminada, no exista, haya cambiado de nombre o no esté disponible temporalmente.</p>
+    <p2 class="output">> Podes intentar volver a <a class="errorLink" href="Index.php?accion=inicio">[Pagina Inicial]</a>.</p>
+    <p3 class="output">> Gracias por la comprensión.</p>
 </div>
 <script src="Assets/js/transicion.js"></script>
 </body>
